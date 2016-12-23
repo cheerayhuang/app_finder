af074992422f54a35460bf4ec4c40d44b9ea6723
Cheeray Huang <cheeray.huang@gmail.com>
2016-12-24 00:07:57 +0800

    add google_play_app table.

--- sql/init.sql
+++ sql/init.sql
@@ -19,3 +19,18 @@ CREATE TABLE IF NOT EXISTS apple_store_app (
     PRIMARY KEY(id),
     INDEX(bundleId, id)
 )ENGINE = innoDB DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS google_play_app (
+    bundleId varchar(64) NOT NULL,
+    trackCensoredName varchar(128) NOT NULL,
+    trackViewUrl varchar(256) NOT NULL,
+    genre varchar(64) NOT NULL,
+    artistName varchar(128) NOT NULL,
+    trackContentRating varchar(32) NOT NULL,
+    averageUserRating float NOT NULL,
+    userRatingCount bigint NOT NULL,
+    id bigint NOT NULL,
+    currency varchar(8) NOT NULL,
+    price float NOT NULL,
+    PRIMARY KEY(bundleId)
+)ENGINE = innoDB DEFAULT CHARACTER SET = utf8;
