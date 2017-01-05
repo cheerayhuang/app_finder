f24a9fe7cf3bb4e44afc4b788d2988153cff5a37
Cheeray Huang <cheeray.huang@gmail.com>
2017-01-05 19:16:06 +0800

    add a sql for postgresql.

---
+++ sql/init_psql.sql
@@ -0,0 +1,39 @@
+CREATE TABLE apple_store_app (
+    id bigint NOT NULL,
+    bundleId varchar(64) NOT NULL,
+    trackCensoredName varchar(128) NOT NULL,
+    trackViewUrl varchar(256) NOT NULL,
+    genre1 varchar(64) NOT NULL,
+    genre2 varchar(64) NOT NULL,
+    genre3 varchar(64) NOT NULL,
+    genre4 varchar(64) NOT NULL,
+    currency varchar(8) NOT NULL,
+    price real NOT NULL,
+    artistId bigint NOT NULL,
+    artistName varchar(128) NOT NULL,
+    sellerName varchar(128) NOT NULL,
+    trackContentRating varchar(8) NOT NULL,
+    averageUserRating real NOT NULL,
+    userRatingCount bigint NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE google_play_app (
+    bundleId varchar(64) NOT NULL,
+    trackCensoredName varchar(128) NOT NULL,
+    trackViewUrl varchar(256) NOT NULL,
+    genre varchar(64) NOT NULL,
+    artistName varchar(128) NOT NULL,
+    trackContentRating varchar(32) NOT NULL,
+    averageUserRating real NOT NULL,
+    userRatingCount bigint NOT NULL,
+    id bigint NOT NULL,
+    currency varchar(8) NOT NULL,
+    price real NOT NULL,
+    PRIMARY KEY(bundleId)
+);
+
+CREATE TABLE not_found_app (
+    bundleId varchar(64) NOT NULL,
+    PRIMARY KEY(bundleId)
+);
