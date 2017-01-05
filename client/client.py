b47b6cc0aa39223704a5684f03d80904834d024d
Cheeray Huang <cheeray.huang@gmail.com>
2017-01-05 19:23:58 +0800

    update exception handler in client.py

--- client/client.py
+++ client/client.py
@@ -19,6 +19,7 @@ def find(args):
 
     total_bundleid = 0
     total_not_found = 0
+    total_err = 0
     d = tree.find('ss:Worksheet/ss:Table/ss:Row/ss:Cell/ss:Data', ns)
     for elem in tree.iterfind('ss:Worksheet/ss:Table/ss:Row', ns):
         for data in elem.iterfind('ss:Cell/ss:Data', ns):
@@ -30,7 +31,8 @@ def find(args):
                 total_bundleid += 1
                 try:
                     r = requests.get(URL_APPLE + text, timeout=10)
-                except requests.exceptions.Timeout:
+                except Exception:
+                    time.sleep(1)
                     continue
                 r_map = r.json()
                 if 'not_access_apple_api' in r_map:
@@ -38,16 +40,23 @@ def find(args):
                 if 'err' in r_map or r_map[text] == "not found":
                     try:
                         r = requests.get(URL_GOOGLE + text, timeout=10)
-                    except requests.exceptions.Timeout:
+                    except Exception:
                         continue
                 print r.text
-                if r.json()[text] == "not found":
+
+                if text in r.json() and r.json()[text] == "not found":
                     total_not_found += 1
-                #diff = time.time() - t1 - 3;
+
+                if 'err' in r.json():
+                    total_err += 1
+
+                diff = time.time() - t1 - 3;
                 #if diff < 0 and not not_access_api:
                 #    time.sleep(math.ceil(-diff))
+
     print 'total: ' + str(total_bundleid)
-    print 'total for "not found": ' + str(total_not_found)
+    print 'total "not found": ' + str(total_not_found)
+    print 'total "errors": ' + str(total_err)
 
 
 
